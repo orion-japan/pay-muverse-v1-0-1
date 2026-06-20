49982ceb28086e5b7e552b62ba0f5b1719a82055
orion-japan <127199400+orion-japan@users.noreply.github.com>
2026-06-20 22:04:36 +0900

    Export Pre-SEED flow directive

--- src/lib/iros/server/preseed/index.ts
+++ src/lib/iros/server/preseed/index.ts
@@ -3,6 +3,6 @@ export * from './detectPreSeedIntent';
 export * from './buildScreenshotDiagnosisSeed';
 export * from './buildScreenshotDiagnosisDirectReply';
 export * from './buildScreenshotDiagnosisPreSeed';
+export * from './buildPreSeedFlowDirective';
 export * from './resolvePreSeedDecision';
 export * from './universal';
-
